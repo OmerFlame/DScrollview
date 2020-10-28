71af01ab0252970c541eaddd596bd02ec52efda3
OmerFlame <renginald.bagel@gmail.com>
2020-10-28 09:08:50 +0200

    Started building Package.swift
    ugh

---
+++ Package.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+
+let package = Package(
+	name: "DScrollView",
+	platforms: [
+		.iOS(.v13)
+	],
+	products: [
+		.library(
+			name: "DScrollView",
+			targets: ["DScrollView"]),
+	],
+	dependencies: [
+		
+	]
+)
